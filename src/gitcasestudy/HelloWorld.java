d475b136af80fbccea2f57c22bb22eda6d2f6a74
sneharobin <sneharobin101@gmail.com>
2023-06-20 13:11:36 +0530

    This is the branch creation and update commit

--- src/gitcasestudy/HelloWorld.java
+++ src/gitcasestudy/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		
+		int a=2, b=3;
+		a = b+a;
+		System.out.println("var a value is " +a);
 	}
 
 }
